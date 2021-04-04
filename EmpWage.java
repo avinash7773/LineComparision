c59854343d9bb3d142611c52279909fa2961d18e
avinash7773 <javinash228@gmail.com>
2021-04-04 10:28:26 +0530

    Calculate EmpWage using switch

--- EmpWage.java
+++ EmpWage.java
@@ -4,20 +4,26 @@ class EmpWage{
 		int is_Part_time = 2;
 		int total_hours;
 		int wage_rate_per_hour = 20;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		int EmpWage;
-		if(empCheck == is_Full_Time){
-			System.out.println("Employee is Full Time");
-			total_hours = 8;
+		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+		//System.out.println(empCheck);
+		switch (empCheck){
+			case 1:
+				System.out.println("Employee is Full Time");
+				total_hours = 8;
+				break;
 
-		}else if(empCheck == is_Part_time){
-			System.out.println("Employee is Part Time");
-			total_hours = 4;
-		}else{
-			total_hours = 0;
-			System.out.println(" Employee is absent");
+			case 2:
+				System.out.println("Employee is Part Time");
+				total_hours = 4;
+				break;
+
+			default:
+				total_hours = 0;
+				System.out.println(" Employee is absent");
+				break;
 		}
-		EmpWage = wage_rate_per_hour * total_hours;
+
+		int EmpWage = wage_rate_per_hour * total_hours;
 		System.out.println("Employee Wage="+EmpWage);
 
 	}
