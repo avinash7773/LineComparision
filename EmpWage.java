ca392dff5ff2faefc2ff99a50c7686054a3f7799
avinash7773 <javinash228@gmail.com>
2021-04-04 09:42:37 +0530

    add part time hours and calculate empWage

--- EmpWage.java
+++ EmpWage.java
@@ -1,15 +1,25 @@
 class EmpWage{
 	public static  void main(String[] argv){
 		int is_Full_Time = 1;
+		int is_Part_time = 2;
+		int total_hours;
 		int wage_rate_per_hour = 20;
-		int total_hours_in_day = 8;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		int EmpWage;
 		if(empCheck == is_Full_Time){
-			EmpWage = wage_rate_per_hour * total_hours_in_day;
-			System.out.println("Employee Daily Wage="+EmpWage);
-		}else
+			System.out.println("Employee is Full Time");
+			total_hours = 8;
+
+		}else if(empCheck == is_Part_time){
+			System.out.println("Employee is Part Time");
+			total_hours = 4;
+		}else{
+			total_hours = 0;
 			System.out.println(" Employee is absent");
+		}
+		EmpWage = wage_rate_per_hour * total_hours;
+		System.out.println("Employee Wage="+EmpWage);
+
 	}
 
 }
