664c57d0871c994933f8fa20d42c637e4411955a
avinash7773 <javinash228@gmail.com>
2021-04-05 12:35:47 +0530

    check line1 is gretter,less or equal to line2

--- LineComp.java
+++ LineComp.java
@@ -3,7 +3,7 @@ class LineComparision{
 	void equality(){
 	//Line_cartision_point;
 	 	int x1=1;
-         	int x2=2;
+         	int x2=-2;
          	int y1=4;
          	int y2=1;
 	//Line2_Cartision_point;
@@ -15,11 +15,14 @@ class LineComparision{
 	 	double Length_of_Line2=Math.sqrt((z2-z1) * (z2-z1) +(s2-s1) * (s2-s1));
 		Double L1=Double.valueOf(Length_of_Line1);
 		Double L2=Double.valueOf(Length_of_Line2);
-         	if(L1.equals(L2)){
-			System.out.println("Lines are equal");
+         	int ret=L1.compareTo(L2);
+		if(ret>=1){
+			System.out.println("Line1 is gretter then Line2");
 		}
-		else{
-			System.out.println("Lines are not equal");
+		else if(ret<0){
+			System.out.println("Line1 are less then Line2");
+		}else if(ret==0){
+			System.out.println("Lines are equals");
 		}
 	}
 	public static void main(String[] argv){
